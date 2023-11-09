3d85e115d92d4e0e2c345590b1f980c9fc598c36
Marlon <90068388+marlon-frazier@users.noreply.github.com>
2023-11-08 22:19:42 -0600

    basic functionality works; need to error handling and persistent data

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
 
 
+
+
+
 def menu():
     print("TODO APPLICATION")
     print("1) Add new task")
@@ -16,26 +19,34 @@ def add_task():
 
 
 def modify_task():
-    pass
+    show_all_tasks()
+    modify = int(input("Which task do you want to modify? "))
+    mod_task = input("What is the updated task? ")
+    tasks[modify - 1] = mod_task
 
 
 def delete_task():
-    pass
+    show_all_tasks()
+    selected_task = int(input("Which task do you want to delete? "))
+    del tasks[selected_task - 1]
 
 
 def show_all_tasks():
-    print(tasks)
+    index = 1
+    for item in tasks:
+        print(f"{index}. {item}")
+        index += 1
 
 
 tasks = []
 
 
-menu()
-choice = int(input("\nPlease make a selection: "))
 while True:
+    menu()
+    choice = int(input("\nPlease make a selection: "))
+
     if choice == 1:
         add_task()
-        continue
     elif choice == 2:
         modify_task()
     elif choice == 3:
